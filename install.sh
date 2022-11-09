47b64d0d39377f1ae05b20e943202751e1f2005e
LJ <117188129+Gweronx@users.noreply.github.com>
2022-11-09 20:57:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 echo "NOTE: install.sh needs the Github CLI to be installed for it to work!"
 cd $HOME
-mkdir Oddsie
-cd Oddsie
 gh repo clone Oddsie/OddsiePlusPlus
 cd OddsiePlusPlus
 chmod +x oddsie
-echo 'export PATH="$HOME/Oddsie/OddsiePlusPlus:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/OddsiePlusPlus:$PATH"' >> ~/.bashrc
