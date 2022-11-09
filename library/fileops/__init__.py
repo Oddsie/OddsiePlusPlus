aeb1ce9aa10ae47e0f25f97cd33f0d40c0c59d94
LJ <117188129+Gweronx@users.noreply.github.com>
2022-11-09 21:12:17 +0300

    Create __init__.py

---
+++ library/fileops/__init__.py
@@ -0,0 +1,10 @@
+class file:
+	def __init__(self,filename):
+		self.name=filename
+	def writeTo(contents):
+		self.f=open(self.name,"a")
+		self.f.write(contents)
+		self.f.close()
+	def readFrom():
+		self.f=open(self.name)
+		return self.f.read()
